2f87efa9301a4b55cc4517c2fa947b3aee15c540
MandrenkoOlexandr <mandrenkos2005@gmail.com>
2024-12-09 13:37:58 +0200

    First version UI.

--- app/src/main/java/com/example/cryptocurrencyexchange/presentation/CurrencyItemActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyexchange/presentation/CurrencyItemActivity.kt
@@ -15,26 +15,26 @@ class CurrencyItemActivity : AppCompatActivity(){
             CurrencyItemFullBinding.inflate(layoutInflater)
         }
 
-//    private val viewModel by viewModels<CurrencyItemViewModel>()
+    private val viewModel by viewModels<CurrencyItemViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
 
-//        setupLiveData()
+        setupLiveData()
     }
 
-//    private fun setupLiveData() {
-//        viewModel.itemLiveData.observe(this) {
-//            with(binding) {
-//                cryptoCurrency.setText(it.currencyName)
-//                rateValue.setText(it.price.toString())
-//                minRateValue.setText(it.min24hour.toString())
-//                maxRateValue.setText(it.max24hour.toString())
-//                lastDealValue.setText(it.lastDeal)
-//                updateValue.setText(it.lastUpdate)
-//           }
-//        }
-//    }
+    private fun setupLiveData() {
+        viewModel.itemLiveData.observe(this) {
+            with(binding) {
+                cryptoCurrency.setText(it.currencyName)
+                rateValue.setText(it.price.toString())
+                minRateValue.setText(it.min24hour.toString())
+                maxRateValue.setText(it.max24hour.toString())
+                lastDealValue.setText(it.lastDeal)
+                updateValue.setText(it.lastUpdate)
+           }
+        }
+    }
 }
--- app/src/main/java/com/example/cryptocurrencyexchange/presentation/CurrencyItemViewModel.kt
+++ app/src/main/java/com/example/cryptocurrencyexchange/presentation/CurrencyItemViewModel.kt
@@ -1,5 +1,32 @@
 package com.example.cryptocurrencyexchange.presentation
 
-class CurrencyItemViewModel {
-    // TODO: ...
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurrencyexchange.domain.CurrencyItem
+import com.example.cryptocurrencyexchange.domain.usecases.GetItemUseCase
+import com.example.cryptocurrencyexchange.domain.usecases.UpdateUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyItemViewModel @Inject constructor(
+    private val updateUseCase: UpdateUseCase,
+    private val getItemUseCase: GetItemUseCase,
+) : ViewModel() {
+
+
+    private val _itemLiveData = MutableLiveData<CurrencyItem>()
+    val itemLiveData: LiveData<CurrencyItem>
+        get() = _itemLiveData
+
+
+    fun getItem(name: String) {
+        viewModelScope.launch {
+            val currencyItem = getItemUseCase(name)
+            _itemLiveData.value = currencyItem
+        }
+    }
 }
