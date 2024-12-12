75b41b86b96b42cc6485ed74836317e6f685b232
MaksymZubarev <maxzub.school@gmail.com>
2024-12-12 03:19:55 +0200

    Cleared Module

--- app/src/main/java/com/example/cryptocurrencyexchange/DI/AppModule.kt
+++ app/src/main/java/com/example/cryptocurrencyexchange/DI/AppModule.kt
@@ -3,15 +3,11 @@ package com.example.cryptocurrencyexchange.DI
 import com.example.cryptocurrencyexchange.data.RepositoryDatabase
 import com.example.cryptocurrencyexchange.data.RepositoryImpl
 import com.example.cryptocurrencyexchange.domain.Repository
-import com.example.cryptocurrencyexchange.domain.usecases.GetItemsUseCase
-import com.example.cryptocurrencyexchange.domain.usecases.UpdateUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -19,26 +15,9 @@ import javax.inject.Singleton
 interface AppModule {
 
 //    @Provides
-//    fun provideRepository(): Repository {
-//        return RepositoryImpl()
-//    }
-//
-//    @Provides
-//    fun provideGetItemsUseCase(repository: Repository): GetItemsUseCase {
-//        return GetItemsUseCase(repository)
-//    }
-//
-//    @Provides
-//    fun provideUpdateUseCase(repository: Repository): UpdateUseCase {
-//        return UpdateUseCase(repository)
-//    }
-//
-//    @Provides
-//    fun provideRetrofit(): Retrofit {
-//        return Retrofit.Builder()
-//            .baseUrl("https://api.example.com/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
+//    @Singleton
+//    fun provideRepository(repository: RepositoryImpl): Repository {
+//        return repository
 //    }
 
     @Binds
